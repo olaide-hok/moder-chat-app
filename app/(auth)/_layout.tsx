e64329adf5c563c32f835427d2a59d1478121822
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-05-18 07:27:10 +0100

    RootLayout

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,21 @@
+import {useUser} from '@clerk/clerk-expo';
+import {Redirect, Stack} from 'expo-router';
+
+export default function RootLayout() {
+    const {isSignedIn} = useUser();
+
+    if (isSignedIn) {
+        return <Redirect href="/(chat)" />;
+    }
+
+    return (
+        <Stack>
+            <Stack.Screen
+                name="index"
+                options={{
+                    headerShown: false,
+                }}
+            />
+        </Stack>
+    );
+}
